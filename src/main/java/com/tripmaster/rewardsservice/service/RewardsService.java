18854a36d8a58bf3d9c14be61d8d2aa5abb3fa55
bamamoudou <mamoudouba8787@gmail.com>
2021-03-03 16:04:47 +0100

    Implement get reward points and trip deal

---
+++ src/main/java/com/tripmaster/rewardsservice/service/RewardsService.java
@@ -0,0 +1,37 @@
+package com.tripmaster.rewardsservice.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import rewardCentral.RewardCentral;
+import tripPricer.Provider;
+import tripPricer.TripPricer;
+
+@Service
+public class RewardsService {
+	private final RewardCentral rewardsCentral;
+	private final TripPricer tripPricer;
+	private final Logger logger = LoggerFactory.getLogger(RewardsService.class);
+
+	@Autowired
+	public RewardsService(RewardCentral rewardCentral, TripPricer tripPricer) {
+		this.rewardsCentral = rewardCentral;
+		this.tripPricer = tripPricer;
+	}
+
+	// TODO: Add async solution? Use WebClient?
+	public int getRewardPoints(UUID attractionId, UUID userId) {
+		return rewardsCentral.getAttractionRewardPoints(attractionId, userId);
+	}
+
+	public List<Provider> getTripDeals(String tripPricerApiKey, UUID userID, int numberOfAdults, int numberOfChildren,
+			int tripDuration, int cumulativeRewardPoints) {
+		return tripPricer.getPrice(tripPricerApiKey, userID, numberOfAdults, numberOfChildren, tripDuration,
+				cumulativeRewardPoints);
+	}
+}
