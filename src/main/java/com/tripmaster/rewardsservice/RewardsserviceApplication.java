428c6bba4d622a800352544f5bb8cfd4fa9fc2e6
bamamoudou <mamoudouba8787@gmail.com>
2021-03-03 16:06:06 +0100

    Adding app configuration

---
+++ src/main/java/com/tripmaster/rewardsservice/RewardsserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tripmaster.rewardsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardsserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tripmaster/rewardsservice/RewardsserviceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tripmaster.rewardsservice;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
