47d64dab55ffa60bb0554ee8f904955f36a08dda
bamamoudou <mamoudouba8787@gmail.com>
2021-03-03 16:02:27 +0100

    Adding bean configuration

---
+++ src/main/java/com/tripmaster/rewardsservice/configuration/RewardsConfig.java
@@ -0,0 +1,19 @@
+package com.tripmaster.rewardsservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import rewardCentral.RewardCentral;
+import tripPricer.TripPricer;
+
+@Configuration
+public class RewardsConfig {
+	@Bean
+	public RewardCentral getRewardCentral() {
+		return new RewardCentral();
+	}
+
+	@Bean
+	public TripPricer getTripPricer() { return new TripPricer();
+	}
+}
