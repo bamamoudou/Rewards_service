b22b89952dddc98b04e56d792f8d8cc0a0795ac1
bamamoudou <mamoudouba8787@gmail.com>
2021-03-09 19:15:07 +0100

    Adding Actuator

---
+++ src/main/java/com/tripmaster/rewardsservice/HealthCheck.java
@@ -0,0 +1,11 @@
+package com.tripmaster.rewardsservice;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+
+public class HealthCheck implements HealthIndicator {
+	@Override
+	public Health health() {
+		return null;
+	}
+}
